2737cc5f4c85ee78d3bd328159c3d9bfdab6483e
Lucia <ldetitto10@gmail.com>
2025-11-01 23:11:58 -0300

    Entrenar artista feature

---
+++ src/main/java/domain/EstadoRol.java
@@ -0,0 +1,5 @@
+package domain;
+
+public enum EstadoRol{
+	BASE, ENTRENAMIENTO;
+}
--- src/main/java/services/CostoEntrenamiento.java
+++ src/main/java/services/CostoEntrenamiento.java
@@ -1,11 +1,29 @@
 package services;
 
-public class CostoEntrenamiento implements Costo {
+import domain.Artista;
+import domain.EstadoRol;
 
-	@Override
-	public double calcularExtra() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+public class CostoEntrenamiento extends CostoDecorator {
+
+	private final Artista artista;
+
+	public CostoEntrenamiento(Costo inner, Artista artista) {
+        super(inner);
+        this.artista = artista;
+    }
+
+    @Override
+    public double calcularExtra() {
+    	double extraAnterior = inner.calcularExtra();
+        boolean tieneEntrenamiento = artista.getRoles().values().stream()
+                .anyMatch(e -> e == EstadoRol.ENTRENAMIENTO);
+        double extraEntrenamiento = 0;
+        
+        if (tieneEntrenamiento) {
+            extraEntrenamiento = artista.getCostoBase() * 0.5;
+        }
+
+        return extraAnterior + extraEntrenamiento;
+    }
 
 }
