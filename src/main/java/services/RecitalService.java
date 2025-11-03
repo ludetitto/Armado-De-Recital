b7648df13b0b7060f27d7f675ada168b4cb8bf83
Lucia <ldetitto10@gmail.com>
2025-11-02 23:11:19 -0300

    RecitalService para listado de canciones y CancionService

--- src/main/java/services/RecitalService.java
+++ src/main/java/services/RecitalService.java
@@ -1,23 +1,34 @@
 package services;
 
+import domain.Artista;
+import domain.Cancion;
+import domain.Contratacion;
+import domain.Recital;
 import domain.TipoRol;
+
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 public class RecitalService {
-	Map<TipoRol, Integer> verRolesFaltantes() {
-		// TODO: implement
-		return null;
+	
+	Map<TipoRol, Integer> verRolesFaltantes(Recital recital) {
+		return recital.getRolesFaltantesTotal();
 	}
 	
-	List<ArtistaService> verArtistasContratados() {
-		// TODO: implement
-		return null;
+	List<Artista> verArtistasContratados(Recital recital) {
+		List<Contratacion> contrataciones = recital.getContrataciones();
+		List<Artista> artistasContratados = new ArrayList<>();
+		for(Contratacion c : contrataciones) {
+			artistasContratados.add(c.getArtista());
+		}
+		
+		return artistasContratados;
 	}
 	
-	List<CancionService> verCanciones() {
-		// TODO: implement
-		return null;
+	Set<Cancion> verCanciones(Recital recital) {
+		return recital.getCanciones();
 	}
 	
 	public void cargarEstadoInicial(String archivo) {}
